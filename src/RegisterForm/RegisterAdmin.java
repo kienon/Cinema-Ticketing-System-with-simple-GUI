c357313cbeb1a08c90898bf6ef615be8a8d9f25b
Ahmad Amsari <35188610+kienon@users.noreply.github.com>
2021-01-07 23:47:48 +0800

    Add files via upload

---
+++ src/RegisterForm/RegisterAdmin.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package RegisterForm;
+
+/**
+ *
+ * @author USER
+ */
+public class RegisterAdmin {
+    
+    public static void main(String[] args){
+        frmAdmin f = new frmAdmin();
+        f.setVisible(true); 
+    }
+    
+    
+}
---
+++ src/RegisterForm/RegisterMovie.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package RegisterForm;
+
+/**
+ *
+ * @author USER
+ */
+public class RegisterMovie {
+    public static void main(String[] args){
+        frmNewMovie info = new frmNewMovie();
+        frmNewMovie.main(null);
+    }
+    
+}
